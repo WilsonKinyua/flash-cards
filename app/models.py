eb3ca66e6fc6c00ee3c47874d7cd3f46737f2f7b
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-25 22:26:42 +0300

    profile model added

--- app/models.py
+++ app/models.py
@@ -37,3 +37,19 @@ class Notes(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+# profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_pic = CloudinaryField('image')
+    contact = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    notes = models.ManyToManyField(Notes, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
