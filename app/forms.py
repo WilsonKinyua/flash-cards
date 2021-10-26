29c2ed8233137d4a9b86ea98dc90cdab4392b776
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-26 13:44:12 +0300

    adding login page

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Subject,Notes
-
-class EntryForm(ModelForm):
-    class Meta:
-        model = Notes
-        fields = ('user','title','description','subject' )
-        exclude = ['created_at','updated_at']
