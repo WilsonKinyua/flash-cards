c0c9525d9a0759a1d835095285150fb5c85ea11e
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-27 00:13:32 +0300

    getting subjects from the api done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponseDataService } from '../services/response-data.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  subjecs;
 
-  constructor() { }
+  constructor(private responseService: ResponseDataService) {}
 
   ngOnInit(): void {
+    this.getSubjects();
   }
 
+  getSubjects() {
+    this.responseService.getSubjects().subscribe((data: any) => {
+      this.subjecs = data;
+      console.log(this.subjecs);
+    });
+  }
 }
